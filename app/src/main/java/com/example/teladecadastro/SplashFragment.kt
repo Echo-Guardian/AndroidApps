9babe9a446768dc450e6c3d42ca573509c75979d
cc23313 <142255258+cc23313@users.noreply.github.com>
2024-06-10 16:42:51 -0300

    echoAtualizado

---
+++ app/src/main/java/com/example/teladecadastro/SplashFragment.kt
@@ -0,0 +1,29 @@
+package com.example.teladecadastro
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class SplashFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_splash, container, false)
+
+        // Delay of 3 seconds
+        Handler(Looper.getMainLooper()).postDelayed({
+            // Start MainActivity after the delay
+            startActivity(Intent(activity, MainActivity::class.java))
+            activity?.finish()
+        }, 3000)
+
+        return view
+    }
+}
