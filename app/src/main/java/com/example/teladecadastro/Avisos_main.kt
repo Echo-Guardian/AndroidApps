80a110a0d0d50f038c65de540842d0403eb33a93
FaMalagodi <141671142+FaMalagodi@users.noreply.github.com>
2024-11-04 20:38:25 -0300

    App atualizado 4/11

--- app/src/main/java/com/example/teladecadastro/Avisos_main.kt
+++ app/src/main/java/com/example/teladecadastro/Avisos_main.kt
@@ -11,7 +11,6 @@ import androidx.appcompat.app.AppCompatActivity
 
 class Avisos_main : AppCompatActivity() {
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.avisos_main)
